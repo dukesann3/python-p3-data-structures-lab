e1dcb53ea808ef63662451d32783899b99879377
Duke <dukemorise33@gmail.com>
2023-11-28 19:38:28 -0800

    completed lesson

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,72 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    spicy_foods_names = []
+    for food in spicy_foods:
+        food_name = food.get("name")
+        spicy_foods_names.append(food_name)
+    
+    return spicy_foods_names
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    foods_that_are_spicy = []
+    for food in spicy_foods:
+        spicy_level = food.get("heat_level")
+        if spicy_level > 5:
+            foods_that_are_spicy.append(food)
+
+    return foods_that_are_spicy
+
+
+def heat_level_converter(spicy_foods_level):
+    chili_string = "🌶"
+    converted_spicy_level = chili_string * spicy_foods_level
+    return converted_spicy_level
+
 
 def print_spicy_foods(spicy_foods):
-    pass
+
+
+    for food in spicy_foods:
+        food_name = food.get("name")
+        food_type = food.get("cuisine")
+        food_spicy_level = food.get("heat_level")
+        converted_food_spicy_level = heat_level_converter(food_spicy_level)
+
+        print(f"{food_name} ({food_type}) | Heat Level: {converted_food_spicy_level}")
+    
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    
+    for food in spicy_foods:
+        cuisine_type = food.get("cuisine")
+        if cuisine_type == cuisine:
+            return food
+
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    spiciest_foods = get_spiciest_foods(spicy_foods)
+    for food in spiciest_foods:
+        food_name = food.get("name")
+        food_cuisine = food.get("cuisine")
+        food_spicy_level = food.get("heat_level")
+        converted_spicy_level = heat_level_converter(food_spicy_level)
+
+        print(f"{food_name} ({food_cuisine}) | Heat Level: {converted_spicy_level}")
+
 
 def get_average_heat_level(spicy_foods):
-    pass
+    sum_of_spicy_levels = 0
+    length_of_spicy_foods_list = len(spicy_foods)
+
+    for food in spicy_foods:
+        food_spicy_level = food.get("heat_level")
+        sum_of_spicy_levels += food_spicy_level
+    
+    average_spicy_level = sum_of_spicy_levels / length_of_spicy_foods_list
+    return average_spicy_level
+
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
